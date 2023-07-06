2ce97075eada698dd1234c43950b8b0977fbfbf4
chomapublicize <choma@publicize.co>
2023-07-06 18:47:26 -0500

    pokemon link list

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   const [pokemonList, setPokemonList] = useState<any>(null);
   return (
     <div className="bg-zinc-900 grid place-content-center min-h-screen text-white">
-      <h1 className="mb-2 italic text-amber-600 text-xl px-5 text-center">Hi master pokemon!</h1>
+      <div className='text-center mb-3'>
+        <h1 className="mb-1 italic text-amber-600 text-xl px-5 text-center">Hi master pokemon!</h1>
+        <a href='https://www.wikidex.net/wiki/Lista_de_Pok%C3%A9mon' target='_blank' className="mb-2 italic text-amber-600 text-xl px-5 text-center">Pokemon List</a>
+      </div>
       <div className="mb-4 w-48 m-auto">
         <Search setPokemonList={setPokemonList}/>
       </div>
